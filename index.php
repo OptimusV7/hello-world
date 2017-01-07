8fa383aad28696be7027fa75b9b0b0819c7c4092
OptimusV7 <manstill09@outlook.com>
2017-01-07 19:23:06 +0300

    Adding files

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <title>  Hello There Whats Up!!!  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link href="hello.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Arima+Madurai" rel="stylesheet">
+</head>
+<body>
+  <div class="background">
+  <img src="img/back1.jpg" alt="">
+  <header> This is the first page to be coded by nearly every developer. So here is mine.
+    <br>Hello world </header>
+  </div>
+</body>
+</html>
